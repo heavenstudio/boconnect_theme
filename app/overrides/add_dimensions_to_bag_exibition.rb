d8e12a37df706647895043d80406262a2c8066bb
Stefano Diem Benatti <stefano.diem@gmail.com>
2011-12-05 12:25:14 -0200

    adding dimensions to product

---
+++ app/overrides/add_dimensions_to_bag_exibition.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+Deface::Override.new virtual_path: 'products/_properties',
+                     name: 'add_dimensions_to_bag_exhibition',
+                     insert_bottom: "#product-properties",
+                     text: %q{<dt>Dimensões ( L x A x P):</dt>
+                              <dd><%= @product.width %> x <%= @product.height %> x <%= @product.depth %></dd>
+                           }
