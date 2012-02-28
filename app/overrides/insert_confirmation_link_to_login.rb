174b733a84547dda482ee0d48917eb48d50bec01
Stefano Diem Benatti <stefano.diem@gmail.com>
2012-02-28 16:02:18 -0300

    Trying to fix encoding issue

--- app/overrides/insert_confirmation_link_to_login.rb
+++ app/overrides/insert_confirmation_link_to_login.rb
@@ -1,3 +1,3 @@
 # encoding: utf-8
 Deface::Override.new(virtual_path: "spree/user_sessions/new", name: "insert_login_confirmation", insert_bottom: "[data-hook=login]",
-                     text: "| <%= link_to \"Reenviar e-mail de confirmação\", new_user_confirmation_path %>")
+                     text: "| <%= link_to \"Reenviar e-mail de confirma&ccedi;&atilde;o\", new_user_confirmation_path %>")
