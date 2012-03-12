f0f5ddd37ce535cc1060d8ce1a7b53a5beba59ae
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-03-11 23:39:16 -0300

    changed layout 2

--- app/overrides/tweak_product_list_item.rb
+++ app/overrides/tweak_product_list_item.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
                      :name => 'tweak_product_list_item',
                      :replace => "[data-hook='products_list_item']",
                      :text => %q{<li id="product_<%= product.id %>" data-hook="products_list_item">
-                                   <%= link_to(product, :class => 'info', :length => 50) do %>
+                                   <%= link_to(product, :class => 'info') do %>
                                      <%= product.name %>
                                      <span class="price selling"><%= number_to_currency(product.price) %></span>
                                      <span class="details"><img src='/assets/details.png'/></span>
