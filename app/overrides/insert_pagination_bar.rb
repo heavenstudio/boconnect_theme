d1ca92f975802f6425f905fc1d098e219c692923
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-03-12 00:01:54 -0300

    changed layout 4

--- app/overrides/insert_pagination_bar.rb
+++ app/overrides/insert_pagination_bar.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products", 
                      :name => "insert_pagination_bar",
                      :insert_before => "ul#products", 
-                     :text => "<div id='pagination_bar'><img class='classificar' src='/assets/banner.png'/></div>")
+                     :text => "<div id='pagination_bar'><img class='classificar' src='/assets/classificar.png'/></div>")
