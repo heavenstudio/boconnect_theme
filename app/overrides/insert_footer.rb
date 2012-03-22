f2703cd47c74c082f009e51b269da03e98e2333e
Thiago Colucci <ticolucci@gmail.com>
2012-03-22 12:51:37 -0300

    inserting real location for footer link

--- app/overrides/insert_footer.rb
+++ app/overrides/insert_footer.rb
@@ -5,11 +5,12 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :text => "
                        <div id='footer_bar'>
                          <ul'>
-                           <li><a href="">FAQ</a></li>
-                           <li><a href="">TERMOS & CONDIÇÕES</a></li>
-                           <li class='last'><a href="">CONTATO</a></li>
-                           <li class='last'><a href=""><img id='facebook' src='/assets/facebook.png'/></a></li>
-                           <li class='last'><a href=""><img id='twitter' src='/assets/twitter.png'/></a></li>
+                           <li><a href='/how_it_works'>COMO FUNCIONA</a></li>
+                           <li><a href=''>FAQ</a></li>
+                           <li><a href='/terms'>TERMOS & CONDIÇÕES</a></li>
+                           <li class='last'><a href=''>CONTATO</a></li>
+                           <li class='last'><a href=''><img id='facebook' src='/assets/facebook.png'/></a></li>
+                           <li class='last'><a href=''><img id='twitter' src='/assets/twitter.png'/></a></li>
                          </ul>
                          <div id='allrights'>BoConnect Corporation - All rights Reserved Tel (55 - 21) 2210-7277 </div>
                          <img id='bobags' src='/assets/bobags.png'/>
