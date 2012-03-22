97c3070d7856231af7f8ed77d0fa2a3664156d19
Stefano Diem Benatti <stefano.diem@gmail.com>
2012-03-22 03:22:56 -0300

    Adding more originals

--- app/overrides/insert_pagination_bar.rb
+++ app/overrides/insert_pagination_bar.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/shared/_products", 
                      :name => "insert_pagination_bar",
                      :insert_before => "ul#products", 
-                     :text => "<div id='pagination_bar'><img class='classificar' src='/assets/classificar.png'/></div>")
+                     :text => "<div id='pagination_bar'><img class='classificar' src='/assets/classificar.png'/></div>",
+                     :original => '250e9a5aeade63e43e78bd927592e2fee6e0edb2')
--- app/overrides/tweak_product_list_item.rb
+++ app/overrides/tweak_product_list_item.rb
@@ -8,4 +8,5 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
                                      <span class="details"><img src='/assets/details.png'/></span>
                                    <% end %>
                                    <%= link_to small_image(product), product %>
-                                 </li>})
+                                 </li>},
+                      :original => 'ac90fd31020f0647c5369ca8e99cf10469cfc0c7')
