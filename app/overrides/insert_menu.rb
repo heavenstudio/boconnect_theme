ab38d00c35d2337c71bb06a3ef4c7a15877bbd2c
Stefano Diem Benatti <stefano.diem@gmail.com>
2012-04-05 17:23:50 -0300

    changing links

--- app/overrides/insert_menu.rb
+++ app/overrides/insert_menu.rb
@@ -4,8 +4,8 @@ Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
                      :text => "
                        <div class='menu container'>
                          <ul id='first'>                         
-                           <li><a href=''><img src='/assets/menu_bags.png'/></a></li>
-                           <li><a href=''><img src='/assets/menu_boconnect.png'/></a></li>
+                           <li><a href='/products'><img src='/assets/menu_bags.png'/></a></li>
+                           <li><a href='/'><img src='/assets/menu_boconnect.png'/></a></li>
                            <li><a href='/how_it_works'><img src='/assets/menu_comofunciona.png'/></a></li>
                            <li><a href='http://www.bobags.com.br/site/index.html'><img src='/assets/menu_bobags.png'/></a></li>
                            <li><a href=''><img src='/assets/menu_contatos.png'/></a></li>
