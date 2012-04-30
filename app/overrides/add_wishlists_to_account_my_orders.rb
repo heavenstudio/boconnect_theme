885fc7dfd3609d237953347cea2a9580dcb1e523
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-04-30 02:42:23 -0300

    bla

--- app/overrides/add_wishlists_to_account_my_orders.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/users/show",
-                     :name => "add_wishlists_to_account_my_orders",
-                     :insert_after => "[data-hook='product_description']",
-                     :partial => "spree/users/wishlists",
-                     :disabled => false)
