1d0f748e7a0e37eafbc59e0d3401de979cca2927
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-05-07 10:03:44 -0300

    add details list

--- app/overrides/tweak_product_list_item.rb
+++ app/overrides/tweak_product_list_item.rb
@@ -2,9 +2,9 @@ Deface::Override.new(:virtual_path => 'spree/shared/_products',
                      :name => 'tweak_product_list_item',
                      :replace => "[data-hook='products_list_item']",
                      :text => %q{<li id="product_<%= product.id %>" data-hook="products_list_item">
-                                   <%= link_to small_image(product), product %>
+                                   <div class='image_thumb'><%= link_to small_image(product), product %></div>  
                                    <%= link_to(product, :class => 'info') do %>
-                                     <span class="name_product"><%= product.name %></span>
+                                     <span class="name_product"><%=h truncate(product.name, :length => 36) %></span>
                                      <span class="price selling"><%= number_to_currency(product.price) %></span>
                                      <span class="details"><img src='/assets/details.png'/></span>
                                    <% end %>
