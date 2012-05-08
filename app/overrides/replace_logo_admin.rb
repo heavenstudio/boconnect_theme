8bf8b9ddff1024fe5c079188898de3dfc7f8b540
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-05-08 00:19:05 -0300

    add home final scrollable

--- app/overrides/replace_logo_admin.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/layouts/admin',
-                    :name => 'replace_logo_admin',
-                    :replace => "a",
-                    :text => "<div id='logo_boconnect' data-hook > <img src='/assets/logo_admin.png'/></div>",
-                    :original => '08968a7c6840795de70b1da6b737c43c0a8b3e12')
