a346674b783f765ae7422a35ff3d9ef2714fcdd5
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-05-25 12:03:10 -0300

    add peso

--- app/overrides/add_dimensions_to_bag_exibition.rb
+++ app/overrides/add_dimensions_to_bag_exibition.rb
@@ -4,4 +4,6 @@ Deface::Override.new virtual_path: 'spree/products/_properties',
                      insert_bottom: "#product-properties",
                      text: %q{<div class='dimension'>Dimensões <span class="detail_dimension">(LxAxP)<span></div>
                               <div class= 'dimension_value'><%= @product.width %> x <%= @product.height %> x <%= @product.depth %> cm</div>
+                              <div class='dimension'>Peso <span class="detail_dimension">(LxAxP)<span></div>
+                              <div class= 'dimension_value'><%= @product.weight %>
                            }
