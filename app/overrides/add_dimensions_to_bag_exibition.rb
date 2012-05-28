9d914f6db48412b0626f8ece1e10c9d5c0c6264f
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-05-28 09:58:18 -0300

    add changes

--- app/overrides/add_dimensions_to_bag_exibition.rb
+++ app/overrides/add_dimensions_to_bag_exibition.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 Deface::Override.new virtual_path: 'spree/products/_properties',
                      name: 'add_dimensions_to_bag_exhibition',
-                     insert_bottom: "#product-properties",
+                     insert_top: "#product-properties",
                      text: %q{<div class='dimension'>Dimensões <span class="detail_dimension">(LxAxP)<span></div>
                               <div class= 'dimension_value'><%= @product.width %> x <%= @product.height %> x <%= @product.depth %> cm</div>
                               <div class='dimension'>Peso</div>
