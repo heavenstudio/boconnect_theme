07fb33e0c026edc730b2d07504a9008cd5d240c7
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-05-28 11:55:26 -0300

    add details pagination

--- app/overrides/insert_pagination_bar.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_products", 
-                     :name => "insert_pagination_bar",
-                     :insert_before => "ul#products", 
-                     :text => "<div id='pagination_bar'></div>",
-                     :original => '250e9a5aeade63e43e78bd927592e2fee6e0edb2')
