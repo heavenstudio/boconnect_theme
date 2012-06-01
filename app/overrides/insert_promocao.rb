1cac6688cf7b8ec0b6f4f63daf2569da5d791509
Kaue Sanchez <kaue_sanchez@yahoo.com>
2012-06-01 16:13:57 -0300

    add details bags tips/promotion

--- app/overrides/insert_promocao.rb
+++ app/overrides/insert_promocao.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/spree_application", 
                       :name => "insert_promocao",
                       :insert_top => "#header", 
-                      :text => "<div id='promocao'><a href=""><img src='/assets/promo.png'/></a></div>",
+                      :text => "<div id='promocao'><a href='/tips'><img src='/assets/bag_tips.png'/></a></div>",
                       :original => 'b0bd3d618ec4c5910dc0db44e73053600cbfebaa')
